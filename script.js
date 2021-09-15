ab0fdb661e9c8f45119e50c14a91ace926db44c0
Maksim <honestliar91@gmail.com>
2021-09-15 15:32:32 +0300

    some fix

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const btn = document.querySelector('.btn__menu'),
 btn.addEventListener('click',()=>{
     menu.classList.toggle('menu--open');
 });
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('menu--open');
+});
+console.log('Выполнил все кроме видео. Использовал следующие семантические теги: aside, footer, header, main, nav, section, time, h1, h3, h4. \n Итоговая оценка 160-10=150 баллов.');
